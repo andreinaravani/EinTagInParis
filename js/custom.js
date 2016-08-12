0c4a364b74b35ae9bd94b37393423e85085cdc03
Nicola Ravani <nicola.ravani@gmail.com>
2016-08-12 21:50:35 +0200

    finishing touches

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,35 @@ $('.carousel-control.left').click(function() {
 $('.carousel-control.right').click(function() {
   $('#myCarousel').carousel('next');
 });
+
+$('a[data-slide="prev"]').click(function() {
+  $('#myCarousel2').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function() {
+  $('#myCarousel2').carousel('next');
+});
+
+$('a[data-slide="prev"]').click(function() {
+  $('#myCarousel3').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function() {
+  $('#myCarousel3').carousel('next');
+});
+
+$('a[data-slide="prev"]').click(function() {
+  $('#myCarousel4').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function() {
+  $('#myCarousel4').carousel('next');
+});
+
+$('a[data-slide="prev"]').click(function() {
+  $('#myCarousel5').carousel('prev');
+});
+
+$('a[data-slide="next"]').click(function() {
+  $('#myCarousel5').carousel('next');
+});
